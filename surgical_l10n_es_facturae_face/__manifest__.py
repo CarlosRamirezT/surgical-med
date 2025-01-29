29801c5b00111f39cfa7f68200a3635bb2ff728b
Carlos Ramírez <carlosdanielrt@gmail.com>
2025-01-29 05:08:38 -0400

    [ADD] surgical_l10n_es_facturae_face: add new module

---
+++ surgical_l10n_es_facturae_face/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Envío de Facturae a FACe (Surgical Med)",
+    "description": "Personalizaciones a la Facturación Electrónica Española para SurgicalMed.es",
+    "summary": "Personalizaciones a la Facturación Electrónica Española para SurgicalMed.es",
+    "version": "17.0.0.0.1",
+    "author": "Carlos Ramírez, Surgical Med",
+    "category": "Accounting & Finance",
+    "website": "https://surgicalmed.es",
+    "depends": [
+        "l10n_es_facturae_face",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "maintainers": ["CarlosRamirezT (carlosdanielrt@gmail.com)",],
+}
