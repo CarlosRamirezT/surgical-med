96d8be21b08261d679dba7a078d3e7a5729a4931
Carlos Ramírez <carlosdanielrt@gmail.com>
2025-04-03 06:18:38 -0400

    ADD quality check integration in stock picking model for enhanced validation

--- surgicalmed_quality_control/models/stock_picking.py
+++ surgicalmed_quality_control/models/stock_picking.py
@@ -1,23 +1,37 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    quality_check_id = fields.Many2one('quality.check', string="Quality Check")
+
+
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     @api.model
     def create(self, vals):
         picking = super(StockPicking, self).create(vals)
+        self._create_quality_checks(picking)
+        return picking
+
+    def button_validate(self):
+        self._validate_quality_checks()
+        return super(StockPicking, self).button_validate()
+    
+    def _create_quality_checks(self, picking):
         if picking.picking_type_id.code in ['incoming', 'internal']:
             for move in picking.move_lines:
-                self.env['quality.check'].create({
+                quality_check = self.env['quality.check'].create({
                     'picking_id': picking.id,
                     'product_id': move.product_id.id,
                     'product_qty': move.product_uom_qty,
                     'state': 'pending',
                 })
-        return picking
-
-    def button_validate(self):
+                move.quality_check_id = quality_check.id
+    
+    def _validate_quality_checks(self):
         for picking in self:
             pending_checks = self.env['quality.check'].search([
                 ('picking_id', '=', picking.id),
@@ -28,4 +42,3 @@ class StockPicking(models.Model):
                 raise ValidationError(_(
                     "No se puede validar el picking porque los siguientes productos tienen quality checks pendientes:\n\n%s"
                 ) % pending_products)
-        return super(StockPicking, self).button_validate()
