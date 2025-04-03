6a45db4669aa4c737ffadb9ef1ddf2003200a514
Carlos Ramirez <c.ramirez@marcos.do>
2025-04-03 10:40:52 -0400

    ADD quality evaluation supplier template line model for structured evaluation criteria

--- surgicalmed_quality_control/models/__init__.py
+++ surgicalmed_quality_control/models/__init__.py
@@ -2,6 +2,7 @@ from . import quality_alert_type
 from . import quality_alert
 from . import quality_action
 from . import quality_evaluation_supplier
+from . import quality_evaluation_supplier_line
 from . import quality_supplier_document
 from . import helpdesk_ticket
 from . import stock_picking
