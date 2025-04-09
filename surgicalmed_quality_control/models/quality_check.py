d765e68ee0763a2e96cebdf4e4b12bf5b5f0455c
Carlos Ramirez <c.ramirez@marcos.do>
2025-04-09 14:56:13 -0400

    [FIX]  surgicalmed_quality_control: quality check and quality point models by adding 'ondelete' behavior for 'lot_id' selection to enhance data integrity

--- surgicalmed_quality_control/models/quality_check.py
+++ surgicalmed_quality_control/models/quality_check.py
@@ -6,13 +6,16 @@ class QualityCheck(models.Model):
 
     measure_on = fields.Selection(
         selection_add=[
-            ("lot_id", "Lot/Serial"), 
+            ("lot_id", "Lot/Serial"),
         ],
         help="""Operation = One quality check is requested at the operation level.
                 Product = A quality check is requested per product.
                 Quantity = A quality check is requested for each new product quantity registered, with partial quantity checks also possible.
                 Lot/Serial = A quality check is requested for each lot/serial number registered, with partial quantity checks also possible.
-            """
+            """,
+        ondelete={
+            "lot_id": "set default",
+        },
     )
     lot_id = fields.Many2one(
         "stock.production.lot",
--- surgicalmed_quality_control/models/quality_point.py
+++ surgicalmed_quality_control/models/quality_point.py
@@ -6,14 +6,17 @@ class QualityPoint(models.Model):
 
     measure_on = fields.Selection(
         selection_add=[
-            ("lot_id", "Lot/Serial"), 
+            ("lot_id", "Lot/Serial"),
         ],
         help="""Operation = One quality check is requested at the operation level.
                   Product = A quality check is requested per product.
                  Quantity = A quality check is requested for each new product quantity registered, with partial quantity checks also possible.
                  Lot/Serial = A quality check is requested for each lot/serial number registered, with partial quantity checks also possible.
                  """,
-        )
+        ondelete={
+            "lot_id": "set default",
+        },
+    )
     
     def _get_checks_values(self, products, company_id, existing_checks=False):
         quality_points_list = []
