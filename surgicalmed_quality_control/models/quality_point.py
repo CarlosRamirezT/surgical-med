45fd25e449b1f6cd766b4c4c63fff81fb92f885e
Carlos Ramirez <c.ramirez@marcos.do>
2025-04-09 15:08:35 -0400

    REFINE quality point model by simplifying the _get_checks_values method and preparing for future logic implementation

--- surgicalmed_quality_control/models/quality_point.py
+++ surgicalmed_quality_control/models/quality_point.py
@@ -19,39 +19,9 @@ class QualityPoint(models.Model):
     )
     
     def _get_checks_values(self, products, company_id, existing_checks=False):
-        quality_points_list = []
-        point_values = []
-        if not existing_checks:
-            existing_checks = []
-        for check in existing_checks:
-            point_key = (check.point_id.id, check.team_id.id, check.product_id.id)
-            quality_points_list.append(point_key)
+        point_values = super(QualityPoint, self)._get_checks_values(products, company_id, existing_checks)
 
-        for point in self:
-            if not point.check_execute_now():
-                continue
-            point_products = point.product_ids
-
-            if point.product_category_ids:
-                point_product_from_categories = self.env['product.product'].search([('categ_id', 'child_of', point.product_category_ids.ids), ('id', 'in', products.ids)])
-                point_products |= point_product_from_categories
-
-            if not point.product_ids and not point.product_category_ids:
-                point_products |= products
-
-            for product in point_products:
-                if product not in products:
-                    continue
-                point_key = (point.id, point.team_id.id, product.id)
-                if point_key in quality_points_list:
-                    continue
-                point_values.append({
-                    'point_id': point.id,
-                    'measure_on': point.measure_on,
-                    'team_id': point.team_id.id,
-                    'product_id': product.id,
-                })
-                quality_points_list.append(point_key)
+        # TODO: implement this logic
 
         return point_values
     
